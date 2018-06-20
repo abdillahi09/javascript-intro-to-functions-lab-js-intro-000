11e94db46f2bc2390a72b4ee5b802331bf73c430
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-06-20 06:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ function logWhisper(string) {
 };
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase()) {
-    return "I can\'t hear you!"
+  if (string === "I love you, Grandma") {
+    return "I love you, too."
   }
-  else if (string === string.toLowerCase()) {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!"
   } else {
-    return undefined
+    return "I can\'t hear you"
   }
 };
 
